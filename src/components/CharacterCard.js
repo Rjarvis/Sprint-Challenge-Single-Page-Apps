8c0192e288086f7325e70d58e3a8d2520aa7e906
Roland J Jarvis <Rjarvis87@gmail.com>
2019-08-19 22:58:19 -0400

    beginning characterCard

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,8 +1,26 @@
 import React from "react";
+import jssPluginPropsSort from "jss-plugin-props-sort";
 
 export default function CharacterCard(props) {
   return (
-    <span>todo: character</span>
+    <Card>
+    <Image src={props.image} wrapped ui={false} />
+    <Card.Content>
+      <Card.Header>{props.name}</Card.Header>
+      <Card.Meta>
+        <span className='date'>{props.species}{props.status}</span>
+      </Card.Meta>
+      <Card.Description>
+        Matthew is a musician living in Nashville.
+      </Card.Description>
+    </Card.Content>
+    <Card.Content extra>
+      <a>
+        <Icon name='user' />
+        22 Friends
+      </a>
+    </Card.Content>
+  </Card>
 
   );
 }
--- src/components/TabNav.js
+++ src/components/TabNav.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Tab, Menu, Icon } from "semantic-ui-react";
 import { NavLink } from "react-router-dom";
+import WelcomePage from "./WelcomePage";
 
 // TODO: Add missing tabs below
 export default function TabNav() {
@@ -8,30 +9,24 @@ export default function TabNav() {
     const panes = [
         {
             menuItem: { key: 'home', icon: 'home', content: 'Home Page'},
-            render: () => <Tab.Pane> <NavLink exact to="/" activeClassName="activeNavButton"/></Tab.Pane>
+            render: () => <Tab.Pane> <NavLink exact to="/" activeClassName="activeNavButton"/><WelcomePage/></Tab.Pane>
         },
         {
             menuItem: { key: 'characters', icon: 'users', content: 'Characters'},
-            render: () => <Tab.Pane><NavLink to='/characters' activeClassName="activeNavButton"/></Tab.Pane>
-        }
-        
+            render: () => <Tab.Pane><NavLink exact to='/characters' activeClassName="activeNavButton"/></Tab.Pane>
+        },
+        {
+            menuItem: { key: 'locations', icon: 'map outline', content: 'Locations'},
+            render: () => <Tab.Pane><NavLink exact to='/locations' activeClassName="activeNavButton"/></Tab.Pane>
+        },
+        {
+            menuItem: { key: 'episodes', icon: 'video', content: 'Episodes'},
+            render: () => <Tab.Pane><NavLink exact to='/episodes' activeClassName="activeNavButton"/></Tab.Pane>
+        }  
     ]
 
     return(
-        <Menu>
-            <Tab panes = {panes}/>
-            <Tab.Pane>
-                <NavLink to="/locations" activeClassName="activeNavButton">
-                    <Icon name='map outline'/>
-                    Locations
-                </NavLink>
-            </Tab.Pane>
-            <Tab.Pane>
-                <NavLink to="Episodes" activeClassName="activeNavButton">
-                    <Icon name='video'/>
-                    Episodes
-                </NavLink>
-            </Tab.Pane>
-        </Menu>
+        <Menu><Tab panes = {panes}/></Menu>
+        
     );
 };
