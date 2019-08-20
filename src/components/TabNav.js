ffb4d6fd3b634d8dc72d425f125e601a045198c5
Roland J Jarvis <Rjarvis87@gmail.com>
2019-08-19 22:10:59 -0400

    starting to make headway on TabNav

--- src/components/TabNav.js
+++ src/components/TabNav.js
@@ -4,7 +4,34 @@ import { NavLink } from "react-router-dom";
 
 // TODO: Add missing tabs below
 export default function TabNav() {
+
+    const panes = [
+        {
+            menuItem: { key: 'home', icon: 'home', content: 'Home Page'},
+            render: () => <Tab.Pane> <NavLink exact to="/" activeClassName="activeNavButton"/></Tab.Pane>
+        },
+        {
+            menuItem: { key: 'characters', icon: 'users', content: 'Characters'},
+            render: () => <Tab.Pane><NavLink to='/characters' activeClassName="activeNavButton"/></Tab.Pane>
+        }
+        
+    ]
+
     return(
-        <Tab>TODO</Tab>
+        <Menu>
+            <Tab panes = {panes}/>
+            <Tab.Pane>
+                <NavLink to="/locations" activeClassName="activeNavButton">
+                    <Icon name='map outline'/>
+                    Locations
+                </NavLink>
+            </Tab.Pane>
+            <Tab.Pane>
+                <NavLink to="Episodes" activeClassName="activeNavButton">
+                    <Icon name='video'/>
+                    Episodes
+                </NavLink>
+            </Tab.Pane>
+        </Menu>
     );
 };
