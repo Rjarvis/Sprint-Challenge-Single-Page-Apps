cdc8a229665f7852c75d219c554afca4811873de
Roland J Jarvis <Rjarvis87@gmail.com>
2019-08-20 21:02:35 -0400

    updoot

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import TabNav from "./components/TabNav.js";
 import Header from "./components/Header.js";
+import Axios from "axios";
+import CharacterCard from './components/CharacterCard.js';
 //import AppRouter from "./components/AppRouter.js";
 
 export default function App() {
+const[data, setData] = useState("");
+useEffect(() => {
+  Axios
+  .get("https://rickandmortyapi.com/api/character/1")
+  .then(res => setData(res.data))
+  .catch(err => console.log(err));
+},[]);
   return (
     <main>
       <Header />
       <TabNav />
+      <CharacterCard props={data}/>
+      {console.log(data)}
+
       {/* TODO<AppRouter /> */}
     </main>
   );
--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,24 +1,27 @@
 import React from "react";
 import jssPluginPropsSort from "jss-plugin-props-sort";
+import {Card, Image, Icon} from 'semantic-ui-react';
 
 export default function CharacterCard(props) {
+  // const location = props.props.location.name;
+  console.log(props.props);
   return (
     <Card>
-    <Image src={props.image} wrapped ui={false} />
-    <Card.Content>
-      <Card.Header>{props.name}</Card.Header>
-      <Card.Meta>
-        <span className='date'>{props.species}{props.status}</span>
-      </Card.Meta>
-      <Card.Description>
-        Matthew is a musician living in Nashville.
-      </Card.Description>
+      <Image src={props.props.image} wrapped ui={true} />
+      <Card.Content>
+        <Card.Header>{props.props.name}</Card.Header>
+        <Card.Meta>
+          <span className='date'>{props.props.species}{props.props.status}</span>
+        </Card.Meta>
+        <Card.Description>
+          {/* {props.props.origin.name} */}
+        </Card.Description>
     </Card.Content>
     <Card.Content extra>
-      <a>
-        <Icon name='user' />
-        22 Friends
-      </a>
+        <a>
+          <Icon name='user' />
+          Episodes
+        </a>
     </Card.Content>
   </Card>
 
